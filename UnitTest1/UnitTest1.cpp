047ec39d359aea36b6a22254b69e8ce3c1079770
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-11-05 20:12:43 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.1 rec/Lab 6.1 rec.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int test[22] = { 31, 11, 22, -6, 23, 28, 14, 0, -4, 28, -9, 29, -5, 27, 15, 21, -5, 18, 33, -2, -3, 12, };
+			int a;
+			a = countElements(test, 22, 0, 0);
+			Assert::AreEqual(a, 18);
+
+			a = sumElements(test, 22, 0, 0);
+			Assert::AreEqual(a, 296);
+		}
+	};
+}
